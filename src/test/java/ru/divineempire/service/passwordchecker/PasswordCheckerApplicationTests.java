60051838be965f8fa9a58a8f3cc30c57353713b2
dmitrykersh <d.karpukhin@mail.ru>
2021-07-01 21:46:10 +0300

    Initial commit

---
+++ src/test/java/ru/divineempire/service/passwordchecker/PasswordCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.divineempire.service.passwordchecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasswordCheckerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
