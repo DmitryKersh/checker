ba9dde0997f726357439a307b10305da8daf3764
dmitrykersh <d.karpukhin@mail.ru>
2021-07-04 21:49:55 +0300

    add: ReplyController class

---
+++ src/main/java/ru/divineempire/service/passwordchecker/ReplyController.java
@@ -0,0 +1,16 @@
+package ru.divineempire.service.passwordchecker;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ReplyController {
+
+    @GetMapping("/")
+    public Reply reply(@RequestParam(value = "login") String login,
+                       @RequestParam(value = "password") String password) {
+        // logic
+        return login.equals(password) ? new Reply(Boolean.FALSE) : new Reply(Boolean.TRUE);
+    }
+}
