d6f4e96b9abce1f239535c42c869760f179a98a1
dmitrykersh <d.karpukhin@mail.ru>
2021-07-07 13:28:38 +0300

    moved: LoginRemoveConfig.java to ...config

--- src/main/java/ru/divineempire/service/passwordchecker/config/LoginRemoveConfig.java
+++ src/main/java/ru/divineempire/service/passwordchecker/config/LoginRemoveConfig.java
@@ -1,4 +1,4 @@
-package ru.divineempire.service.passwordchecker;
+package ru.divineempire.service.passwordchecker.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
