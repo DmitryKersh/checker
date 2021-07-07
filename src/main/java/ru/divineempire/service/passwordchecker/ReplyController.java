77452646681f13a02a9f3229c04f58748eda01be
dmitrykersh <d.karpukhin@mail.ru>
2021-07-07 13:26:38 +0300

    add: error reply changed: main url / -> /check

--- src/main/java/ru/divineempire/service/passwordchecker/ReplyController.java
+++ src/main/java/ru/divineempire/service/passwordchecker/ReplyController.java
@@ -7,10 +7,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ReplyController {
 
-    @GetMapping("/")
+    @GetMapping("/check")
     public Reply reply(@RequestParam(value = "login") String login,
                        @RequestParam(value = "password") String password) {
         // logic
         return login.equals(password) ? new Reply(Boolean.FALSE) : new Reply(Boolean.TRUE);
+    }
+
+    @GetMapping("/error")
+    public String error(){
+        return "error";
     }
 }
