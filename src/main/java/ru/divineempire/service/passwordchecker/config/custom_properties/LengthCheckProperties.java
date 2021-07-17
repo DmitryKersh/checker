1930ab6f528f34c93d6f0d34a8cf9647929f7faa
DmitryKersh <d.karpukhin@mail.ru>
2021-07-18 00:04:34 +0300

    replaced @Getter's and @Setter's with @Data

--- src/main/java/ru/divineempire/service/passwordchecker/config/custom_properties/LengthCheckProperties.java
+++ src/main/java/ru/divineempire/service/passwordchecker/config/custom_properties/LengthCheckProperties.java
@@ -1,14 +1,11 @@
 package ru.divineempire.service.passwordchecker.config.custom_properties;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties("app.lengthcheck")
+@Data
 public class LengthCheckProperties {
-    @Getter @Setter
     private int min = 8;
-
-    @Getter @Setter
     private int max = 30;
 }
