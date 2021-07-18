046ce75a1c4cd6b127a2586e2e3f1014162e0071
DmitryKersh <d.karpukhin@mail.ru>
2021-07-18 21:26:07 +0300

    added: Spring Bean for checksList instead of creating it each /check call empty messages now are not added to list

---
+++ src/main/java/ru/divineempire/service/passwordchecker/config/ChecksConfig.java
@@ -0,0 +1,32 @@
+package ru.divineempire.service.passwordchecker.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import ru.divineempire.service.passwordchecker.checks.DatabaseCheck;
+import ru.divineempire.service.passwordchecker.checks.LengthCheck;
+import ru.divineempire.service.passwordchecker.checks.LoginAsSubstringCheck;
+import ru.divineempire.service.passwordchecker.checks.SpecialSymbolsCheck;
+import ru.divineempire.service.passwordchecker.checks.howtocallit.BasicCheck;
+import ru.divineempire.service.passwordchecker.config.custom_properties.LengthCheckProperties;
+import ru.divineempire.service.passwordchecker.repos.LoginAndPasswordRepository;
+import ru.divineempire.service.passwordchecker.repos.PasswordRepository;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+public class ChecksConfig {
+    @Bean
+    List<BasicCheck> checksList(
+        PasswordRepository passwordRepository,
+        LoginAndPasswordRepository loginAndPasswordRepository,
+        LengthCheckProperties lengthCheckProperties
+    ){
+        return Arrays.asList(
+                new LoginAsSubstringCheck(),
+                new SpecialSymbolsCheck(),
+                new DatabaseCheck(passwordRepository, loginAndPasswordRepository),
+                new LengthCheck(lengthCheckProperties)
+        );
+    }
+}
--- src/main/java/ru/divineempire/service/passwordchecker/web/ReplyController.java
+++ src/main/java/ru/divineempire/service/passwordchecker/web/ReplyController.java
@@ -1,24 +1,15 @@
 package ru.divineempire.service.passwordchecker.web;
 
 import lombok.AccessLevel;
-import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import ru.divineempire.service.passwordchecker.checks.LengthCheck;
 import ru.divineempire.service.passwordchecker.checks.howtocallit.BasicCheck;
-import ru.divineempire.service.passwordchecker.checks.DatabaseCheck;
-import ru.divineempire.service.passwordchecker.checks.LoginAsSubstringCheck;
-import ru.divineempire.service.passwordchecker.checks.SpecialSymbolsCheck;
 import ru.divineempire.service.passwordchecker.checks.howtocallit.CheckResult;
-import ru.divineempire.service.passwordchecker.config.custom_properties.LengthCheckProperties;
-import ru.divineempire.service.passwordchecker.repos.LoginAndPasswordRepository;
-import ru.divineempire.service.passwordchecker.repos.PasswordRepository;
 
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -26,28 +17,19 @@ import java.util.List;
 @FieldDefaults(makeFinal = true)
 @RestController
 public class ReplyController {
-    private PasswordRepository passwordRepository;
-    private LoginAndPasswordRepository loginAndPasswordRepository;
-    private LengthCheckProperties lengthCheckProperties;
+    private List<BasicCheck> checksList;
 
 
     @GetMapping(value = "/check", produces = MediaType.APPLICATION_JSON_VALUE)
     public Reply reply(@RequestParam("login") String login,
                        @RequestParam("password") String password) {
-        List<BasicCheck> checks = Arrays.asList(
-                new LoginAsSubstringCheck(),
-                new SpecialSymbolsCheck(),
-                new DatabaseCheck(passwordRepository, loginAndPasswordRepository),
-                new LengthCheck(lengthCheckProperties)
-        );
-
         int successes = 0;
         int fails = 0;
         List<String> errorMessages = new LinkedList<>();
         List<String> successfulChecks = new LinkedList<>();
         List<String> failedChecks = new LinkedList<>();
 
-        for (BasicCheck check : checks) {
+        for (BasicCheck check : checksList) {
             CheckResult result = check.run(login, password);
 
             if (result.isPassed()) {
@@ -59,24 +41,14 @@ public class ReplyController {
                 failedChecks.add(check.name());
             }
 
-            errorMessages.add(result.getMessage());
+            if (!result.getMessage().isEmpty())
+                errorMessages.add(result.getMessage());
         }
         return new Reply(fails == 0, successes, fails, successfulChecks, failedChecks, errorMessages);
     }
 
-    /*
-        @GetMapping(value = "/checkraw")
-        public String rawreply(@RequestParam("login") String login,
-                               @RequestParam("password") String password) {
-            return (loginAndPasswordRepository.checkLoginPassword(login, password)
-                    && !login.equals(password))
-                    ? "true"
-                    : "false";
-        }
-    */
     @GetMapping("/error")
     public String error() {
         return "error";
-
     }
 }
