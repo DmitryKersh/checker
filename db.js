25f8a17372c76cb470434804d4430070e885891f
dmitrykersh <d.karpukhin@mail.ru>
2021-08-04 12:24:53 +0300

    this might fix tests (12)

--- db.js
+++ db.js
@@ -5,18 +5,32 @@ const pgclient = new Client({
     port: process.env.POSTGRES_PORT,
     user: 'postgres',
     password: 'postgres',
-    database: 'checkerdb'
+    database: 'postgres'
 });
 
 pgclient.connect();
 
+pgclient.query('CREATE DATABASE checkerdb', function(err) { // create user's db
+    if (err)
+        console.log('ignoring the error'); // ignore if the db is there
+    pgclient.end(); // close the connection
+});
+
+const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: 'postgres',
+    password: 'postgres',
+    database: 'checkerdb'
+});
+
 const passwords = 'CREATE TABLE passwords(id BIGSERIAL PRIMARY KEY, password VARCHAR(50) NOT NULL)'
 const login_and_passwords = 'CREATE TABLE login_and_passwords(id BIGSERIAL PRIMARY KEY, password VARCHAR(50) NOT NULL, login VARCHAR(50) NOT NULL)'
 
-pgclient.query(passwords, (err, res) => {
+client.query(passwords, (err, res) => {
     if (err) throw err
 });
 
-pgclient.query(login_and_passwords, (err, res) => {
+client.query(login_and_passwords, (err, res) => {
     if (err) throw err
 });
